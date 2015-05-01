0c69905257753bcf08aac8ced2343ff210b6a1c2
kurttheviking <kurttheviking@outlook.com>
2015-04-30 18:13:54 -0700

    tick patch in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='agileid',
     packages=['agileid'],
-    version='0.1.0',
+    version='0.1.1',
     description="Generate and manage AgileID identifiers",
     url='https://github.com/kurttheviking/agileid-py',
     author='Kurt Ericson',
